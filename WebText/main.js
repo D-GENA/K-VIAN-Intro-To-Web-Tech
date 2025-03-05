0eedd3885de69e6e7225e671e671110a808523aa
D-DENA <kavianblades14@gmail.com>
2025-03-04 19:48:17 -0500

    First Draft

---
+++ WebText/main.js
@@ -0,0 +1,29 @@
+let toggledStatus = false;
+
+
+
+function footerNavShow() {
+  let toggleBtn = document.querySelector('#toggleBtn');
+  let getFooter = document.querySelector('footer');
+  
+
+ if (toggledStatus == false) {
+  
+   
+   getFooter.style.opacity = '3';
+   getFooter.style.visibility = 'visible';
+   
+   
+   toggledStatus = true;
+ }else{
+   
+   getFooter.style.opacity = '0';
+   getFooter.style.visibility = 'hidden';
+   getFooter.style.transition = '.12s ease-in-out';
+   
+
+   
+   toggledStatus = false;
+ }
+
+};
